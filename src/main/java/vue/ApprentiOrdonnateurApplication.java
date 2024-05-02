346fb50a966dc3ee65670d5ca1d5057ac4413558
LAUDAT <seraphin.laudat@gmail.com>
2024-05-02 15:09:07 +0200

    Modification de la taille de la scène

--- src/main/java/vue/ApprentiOrdonnateurApplication.java
+++ src/main/java/vue/ApprentiOrdonnateurApplication.java
@@ -12,7 +12,7 @@ import java.io.FileNotFoundException;
 public class ApprentiOrdonnateurApplication extends Application {
     public void start(Stage stage) throws FileNotFoundException, ExceptionApprentiOrdonnateur {
         VBox root = new VBoxRoot();
-        Scene scene = new Scene(root, 1000, 920);
+        Scene scene = new Scene(root, 875, 925);
 //        File[] fichiersCss = new File("css").listFiles();
 //        for (File fichier : fichiersCss) {
 //            scene.getStylesheets().add(fichier.toURI().toString());
