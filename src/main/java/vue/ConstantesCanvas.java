dd8ddee033d9951f2125911ac65c78ef5701c456
LAUDAT <seraphin.laudat@gmail.com>
2024-05-02 15:05:58 +0200

    Importation de l'image de notre Apprenti

--- src/main/java/vue/ConstantesCanvas.java
+++ src/main/java/vue/ConstantesCanvas.java
@@ -7,9 +7,9 @@ public interface ConstantesCanvas {
     Color COULEUR_APPRENTI = Color.RED;
     Color [] COULEUR_TEMPLE = {Color.GREEN, Color.PINK, Color.ORANGE, Color.VIOLET, Color.BLUE, Color.YELLOW, Color.BROWN};
     Color [] COULEUR_CRISTAL = {Color.GREEN, Color.PINK, Color.ORANGE, Color.VIOLET, Color.BLUE, Color.YELLOW, Color.BROWN};
-    int LARGEUR_CANVAS = 600;
-    int HAUTEUR_CANVAS = 600;
-    int CARRE = 30;
+    int LARGEUR_CANVAS = 800;
+    int HAUTEUR_CANVAS = 800;
+    int CARRE = 25;
     int LARGEUR_OVALE = 19;
     int HAUTEUR_OVALE = 19;
 
