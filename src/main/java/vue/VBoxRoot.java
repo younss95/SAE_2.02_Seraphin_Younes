ae72d72e894b4aca25c9078770b0615e4e1c5833
LAUDAT <seraphin.laudat@gmail.com>
2024-05-06 10:42:47 +0200

    Modification de la classe VBoxRoot

--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -1,12 +1,19 @@
 package vue;
 
+import Controleur.Controleur;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import modele.ApprentiOrdonnateur;
 import modele.ExceptionApprentiOrdonnateur;
 import modele.Position;
 import modele.Temple;
@@ -16,138 +23,42 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class VBoxRoot extends VBox implements ConstantesCanvas{
-    private Label labelNombreDePas;
-    private Canvas canvasCarte;
-    private GraphicsContext graphicsContext2D;
-    private Position positionApprenti;
+public class VBoxRoot extends VBox implements ConstantesCanvas {
+    private static ApprentiOrdonnateur apprenti;
+    private static Controleur controleur;
+    private static VBoxCanva canva;
 
-//    private Image imagePersonnage;
-    private ArrayList<Temple> listTemple = new ArrayList<>();
+    public VBoxRoot() {
+        apprenti = new ApprentiOrdonnateur();
+        controleur = new Controleur();
 
-    public VBoxRoot() throws FileNotFoundException, ExceptionApprentiOrdonnateur {
-//        l'étiquette qui affiche le nombre de pas
-        labelNombreDePas = new Label("Nombre de pas : 0");
+//        la barre des menus
+        MenuBar menuBar = new MenuBar();
+        this.getChildren().add(menuBar);
+        VBox.setMargin(menuBar, new Insets(9));
 
-//        le Canvas et son contexte graphique
-        canvasCarte = new Canvas();
-        canvasCarte.setWidth(LARGEUR_CANVAS);
-        canvasCarte.setHeight(HAUTEUR_CANVAS);
-        graphicsContext2D = canvasCarte.getGraphicsContext2D();
+//        Le menu des scénarios
+        Menu menuScenario = new Menu(INTITULE_MENU_SCENARIOS);
+        menuBar.getMenus().add(menuScenario);
 
-//        les carrés
-        graphicsContext2D.setStroke(COULEUR_GRILLE);
-        for (int i = 0; i < LARGEUR_CANVAS ; i += CARRE) {
-            for (int j = 0; j < HAUTEUR_CANVAS; j += CARRE) {
-                graphicsContext2D.strokeRect(i, j, CARRE, CARRE);
-            }
+//        Les items du menu scénario
+        File [] scenarios = new File("data").listFiles();
+        for (int i = 0; i < scenarios.length; i++) {
+            MenuItem menuItem = new MenuItem(scenarios[i].getName());
+            menuItem.setUserData(scenarios[i]);
+            menuItem.setOnAction(controleur);
+            menuScenario.getItems().add(menuItem);
         }
-//        les numéros de colonne
-        int numCol = -15;
-        graphicsContext2D.setFill(COULEUR_GRILLE);
-        for (int i = CARRE; i < LARGEUR_CANVAS; i += CARRE) {
-            graphicsContext2D.fillText(Integer.toString(numCol), i+CARRE/3, CARRE/2);
-            numCol++;
+        try {
+            canva = new VBoxCanva();
+        } catch (FileNotFoundException | ExceptionApprentiOrdonnateur e) {
+            throw new RuntimeException(e);
         }
-//        les numuméros de ligne
-        int numLigne = -15;
-        graphicsContext2D.setFill(COULEUR_GRILLE);
-        for (int i = CARRE; i < HAUTEUR_CANVAS; i += CARRE) {
-            graphicsContext2D.fillText(Integer.toString(numLigne), CARRE/3, i+CARRE/2);
-            numLigne++;
-        }
-
-//        Lecture fichier
-        Scanner scanner = new Scanner(new File("data" + File.separator + "scenario.txt")).useDelimiter(",");
-        while(scanner.hasNext()) {
-            int abscisse = scanner.nextInt();
-            int ordonne = scanner.nextInt();
-            int coulTemp = scanner.nextInt();
-            int coulCrist = scanner.nextInt();
-
-            if (coulTemp >= 0 && coulCrist >= 0) {
-                Position posiTemple = new Position(abscisse, ordonne);
-                Temple temple = new Temple(posiTemple, coulTemp, coulCrist);
-                System.out.println(temple);
-                listTemple.add(temple);
-            }
-            if (scanner.hasNextLine()) {
-                scanner.nextLine();
-            }
-        }
-        System.out.println(listTemple);
-
-//        Placement des temples sur la map
-        for (Temple temple :listTemple) {
-            graphicsContext2D.setFill(COULEUR_TEMPLE[temple.getCoulTemple()]);
-            graphicsContext2D.fillRect(temple.getPosiTemple().getAbscisse()*CARRE+1, temple.getPosiTemple().getOrdonnee()*CARRE+1, CARRE-2, CARRE-2);
-        }
-        System.out.println(listTemple);
-
-//        Position apprenti
-        positionApprenti = new Position(1, 1);
-        graphicsContext2D.setFill(COULEUR_APPRENTI);
-        graphicsContext2D.fillOval(positionApprenti.getAbscisse()*CARRE + 1 + CARRE/8,
-                positionApprenti.getOrdonnee()*CARRE + CARRE/4, LARGEUR_IMAGE_APP, HAUTEUR_IMAGE_APP);
-
-        canvasCarte.setOnMouseClicked(event -> {
-        int abscisse = (int) event.getX() / CARRE;
-        int ordonnee = (int) event.getY() / CARRE;
-        Position positionCliquee = new Position(abscisse, ordonnee);
-        graphicsContext2D.fillRect(abscisse*CARRE+1, ordonnee*CARRE+1, CARRE-2, CARRE-2);
-        System.out.println(positionCliquee);
-        deplacementAvecTimer(positionCliquee, positionApprenti);
-        });
-
-//        ajout des composants graphiques à la racine (this)
-        this.getChildren().add(labelNombreDePas);
-        VBox.setMargin(labelNombreDePas, new Insets(30));
-        this.getChildren().add(canvasCarte);
-        VBox.setMargin(canvasCarte, new Insets(30));
     }
-//        Déplacement de l'apprenti
-    /**
-     * La méthode déplace l'apprenti vers la position de la case saisie en paramètre
-     * @param positionCliquee Position : position obtenu en cliquant sur une case
-     * @param positionApprenti Position : position de l'apprenti
-     */
-    private void deplacementAvecTimer(Position positionCliquee, Position positionApprenti) {
-        Timer timer = new Timer();
-        TimerTask timerTask = new TimerTask() {
-            @Override
-            public void run() {
-//                ordoImp.setImage(null);
 
-//                Effacer le personnage de la case
-                graphicsContext2D.clearRect(positionApprenti.getAbscisse() * CARRE+2, positionApprenti.getOrdonnee() * CARRE+2, CARRE-3, CARRE-3);
-
-//                redessiner le temple sur la position effacée
-                for (Temple temple : listTemple) {
-                    if (positionApprenti.equals(temple.getPosiTemple())) {
-                        graphicsContext2D.setFill(COULEUR_TEMPLE[temple.getCoulTemple()]);
-                        graphicsContext2D.fillRect(temple.getPosiTemple().getAbscisse()*CARRE+1, temple.getPosiTemple().getOrdonnee()*CARRE+1, CARRE-2, CARRE-2);
-                        break;
-                    }
-                }
-//                System.out.println(listTemple);
-
-//                deplacement du personnage
-                Image imagePersonnage = new Image("F:\\SAE\\SAE 2.02 Exploration algorithmique\\TP-SAE\\Image\\ApprentiImage.png");
-                positionApprenti.deplacementUneCase(positionCliquee);
-                ImageView en_mouv = new ImageView(imagePersonnage);
-
-//                dessin du perso sur la nouvelle position
-                graphicsContext2D.drawImage(en_mouv.getImage(), positionApprenti.getAbscisse() * CARRE + 1 + CARRE / 8, positionApprenti.getOrdonnee() * CARRE + CARRE / 4, LARGEUR_IMAGE_APP, HAUTEUR_IMAGE_APP);
-                if (positionApprenti.equals(positionCliquee)) {
-                    timer.cancel();
-                }
-
-            Platform.runLater(() -> {
-                labelNombreDePas.setText("Nombre de pas : " + Position.getNombreDePas());
-            });
-            }
-        };
-        timer.scheduleAtFixedRate(timerTask, 1000, 200);
-
+    public static ApprentiOrdonnateur getApprenti() {
+        return apprenti;
     }
+
+
 }
