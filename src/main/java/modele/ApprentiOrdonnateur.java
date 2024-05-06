228f3e5ec06f5131aa48f7b6a4a8d580f69573b3
LAUDAT <seraphin.laudat@gmail.com>
2024-05-06 15:15:30 +0200

    Ajout d'accesseurs et geter

--- src/main/java/modele/ApprentiOrdonnateur.java
+++ src/main/java/modele/ApprentiOrdonnateur.java
@@ -6,18 +6,27 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class ApprentiOrdonnateur implements ConstantesCanvas {
-    private Position positionApprenti;
+    private static Position positionApprenti;
     private Collection<Temple> listTemples = new ArrayList<>();
 
     public ApprentiOrdonnateur(){
-        positionApprenti = new Position(LARGEUR_CANVAS/2, HAUTEUR_CANVAS/2);
+        positionApprenti = new Position((LARGEUR_CANVAS/CARRE)/2, (HAUTEUR_CANVAS/CARRE)/2);
     }
 
+
+//    Les trucs
     public void setTemples(Collection<Temple> temples) {
         listTemples = temples;
     }
     public void setPositionApprenti(Position parPosition) {
         positionApprenti = parPosition;
+    }
+//    Les accesseurs
+    public static Position getPositionApprenti() {
+        return positionApprenti;
+    }
+    public Collection<Temple> getListTemples() {
+        return listTemples;
     }
 
     public String toString(){
