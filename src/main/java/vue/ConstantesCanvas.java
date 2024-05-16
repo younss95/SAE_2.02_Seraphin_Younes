b3a0e17a1fbd5c423b9101a3f68684c4b8bc95a4
LAUDAT <seraphin.laudat@gmail.com>
2024-05-16 14:56:05 +0200

    Rajout de couleurs car insuffisantes lors de la lecture de fichier

--- src/main/java/vue/ConstantesCanvas.java
+++ src/main/java/vue/ConstantesCanvas.java
@@ -6,13 +6,13 @@ public interface ConstantesCanvas {
     String INTITULE_MENU_SCENARIOS = "Scénario";
     Color COULEUR_GRILLE = Color.BLACK;
     Color COULEUR_APPRENTI = Color.RED;
-    Color [] COULEUR_TEMPLE = {Color.GREEN, Color.PINK, Color.ORANGE, Color.VIOLET, Color.BLUE, Color.YELLOW, Color.BROWN};
-    Color [] COULEUR_CRISTAL = {Color.GREEN, Color.PINK, Color.ORANGE, Color.VIOLET, Color.BLUE, Color.YELLOW, Color.BROWN};
+    Color [] COULEUR_TEMPLE = {Color.GRAY, Color.BROWN, Color.PINK, Color.ORANGE, Color.VIOLET, Color.BLUE, Color.YELLOW, Color.GREEN, Color.PURPLE, Color.CYAN};
+    Color [] COULEUR_CRISTAL = {Color.GRAY, Color.BROWN, Color.PINK, Color.ORANGE, Color.VIOLET, Color.BLUE, Color.YELLOW, Color.GREEN, Color.PURPLE, Color.CYAN};
     int LARGEUR_CANVAS = 800;
     int HAUTEUR_CANVAS = 800;
     int CARRE = 25;
-    int LARGEUR_IMAGE_APP = 20;
-    int HAUTEUR_IMAGE_APP = 20;
+    int LARGEUR_IMAGE_APP = 25;
+    int HAUTEUR_IMAGE_APP = 25;
 
 
 }
