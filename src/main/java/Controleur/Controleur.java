acb84b59b1efca04e4f9f24aa61f8060120436f7
Seraphinldt <seraphin.laudat@gmail.com>
2024-05-24 11:45:17 +0200

    Modification 'Controleur' : Evenement echange bouton finalisée exception : renvoie la reference de l'objet et non l'objet lui-même

--- src/main/java/Controleur/Controleur.java
+++ src/main/java/Controleur/Controleur.java
@@ -5,10 +5,7 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
-import modele.ApprentiOrdonnateur;
-import modele.ExceptionApprentiOrdonnateur;
-import modele.Position;
-import modele.Temple;
+import modele.*;
 import vue.ConstantesCanvas;
 import vue.LectureScenario;
 import vue.VBoxRoot;
@@ -33,34 +30,85 @@ public class Controleur implements EventHandler, ConstantesCanvas {
                 System.out.println(VBoxRoot.getApprenti().getListTemples());
             }
         }
-//        Object userData = ()
-        if(event.getSource() instanceof Button){
-            Button bouton = (Button) event.getSource();
-            if(bouton.getText().equals("Echanger cristal")){
-                Color coulCristalApp = VBoxRoot.getApprenti().getCoulCristal(), varTmpApp = VBoxRoot.getApprenti().getCoulCristal();
+
+//        if(event.getSource() instanceof Button){
+//            Button bouton = (Button) event.getSource();
+//            if(bouton.getText().equals("Echanger cristal")){
+//                Color coulCristalApp = VBoxRoot.getApprenti().getCoulCristal(), varTmpApp = VBoxRoot.getApprenti().getCoulCristal();
+//                Position posiApprenti = ApprentiOrdonnateur.getPositionApprenti();
+//                Temple templeApprenti = null;
+//                for (Temple temple : VBoxRoot.getApprenti().getListTemples()) {
+//                    if (temple.getPosiTemple().equals(posiApprenti)) {
+//                        templeApprenti = temple;
+//                        break;
+//                    }
+//                }
+////                ne fonctionne pas, il faut acceder a la valeur et non à l'adresse mémoir où est stockée l'info
+//                assert templeApprenti != null;
+//                Color coulCristalTemple = COULEUR_CRISTAL[templeApprenti.getCoulCristal()];
+//                System.out.println(coulCristalApp);
+//                System.out.println(coulCristalTemple);
+//                coulCristalApp = coulCristalTemple;
+//                coulCristalTemple = varTmpApp;
+//                System.out.println(coulCristalApp);
+//                System.out.println(coulCristalTemple);
+//
+//                System.out.println("Cristal echange");
+//
+//
+//            }
+//        }
+
+//        Object userData = ();
+        if (event.getSource() instanceof Button bouton) {
+            if (bouton.getUserData() instanceof String) {
+
+                // Récupérer l'apprenti et sa couleur de cristal actuelle
+                ApprentiOrdonnateur apprenti = VBoxRoot.getApprenti();
+                Cristal cristalApp = apprenti.getCristal();
+                Cristal cristalAppTmps = apprenti.getCristal();
                 Position posiApprenti = ApprentiOrdonnateur.getPositionApprenti();
+
+                // Trouver le temple où se trouve l'apprenti
                 Temple templeApprenti = null;
-                for (Temple temple : VBoxRoot.getApprenti().getListTemples()) {
+                for (Temple temple : apprenti.getListTemples()) {
                     if (temple.getPosiTemple().equals(posiApprenti)) {
                         templeApprenti = temple;
-                        break;
+//                        break;
                     }
                 }
-//                ne fonctionne pas, il faut acceder a la valeur et non à l'adresse mémoir où est stockée l'info
+
+                // S'assurer que le temple a été trouvé
                 assert templeApprenti != null;
-                Color coulCristalTemple = COULEUR_CRISTAL[templeApprenti.getCoulCristal()];
-                System.out.println(coulCristalApp);
-                System.out.println(coulCristalTemple);
-                coulCristalApp = coulCristalTemple;
-                coulCristalTemple = varTmpApp;
-                System.out.println(coulCristalApp);
-                System.out.println(coulCristalTemple);
 
-                System.out.println("Cristal echange");
+                // Récupérer la couleur du cristal du temple
+                Cristal cristalTemple = Temple.getCristal();
 
+                // Afficher les couleurs avant l'échange
+                System.out.println("Avant l'échange :");
+                System.out.println("Couleur Cristal Apprenti : " + COULEUR_CRISTAL[cristalApp.getCoulCristal()]);
+                System.out.println("Couleur Cristal Temple : " + COULEUR_CRISTAL[cristalTemple.getCoulCristal()]);
 
+                // Échanger les couleurs
+                apprenti.setCristal(cristalTemple);
+                templeApprenti.setCristal(cristalAppTmps);
+
+                // Afficher les couleurs après l'échange
+                System.out.println("Après l'échange :");
+                System.out.println("Couleur Cristal Apprenti : " + COULEUR_CRISTAL[cristalApp.getCoulCristal()]);
+                System.out.println("Couleur Cristal Temple : " + COULEUR_CRISTAL[cristalTemple.getCoulCristal()]);
+
+//                System.out.println("Cristal échangé");
             }
         }
 
     }
+//    private int findColorIndex(Color color) {
+//        for (int i = 0; i < COULEUR_CRISTAL.length; i++) {
+//            if (COULEUR_CRISTAL[i].equals(color)) {
+//                return i;
+//            }
+//        }
+//        return -1; // ou lever une exception si la couleur n'est pas trouvée
+//    }
 }
