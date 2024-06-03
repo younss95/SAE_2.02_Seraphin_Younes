51e2d4d674ff512a20881d97a9031325a59087ae
younss95 <163010503+younss95@users.noreply.github.com>
2024-06-03 09:37:57 +0200

    doc string de la classe AlgoTri

--- src/main/java/algorithme/AlgoTri.java
+++ src/main/java/algorithme/AlgoTri.java
@@ -8,10 +8,18 @@ import vue.VBoxRoot;
 
 import java.util.Collection;
 
+/**
+ * La classe AlgoTri implémente l'algorithme de tri pour un apprenti dans un temple.
+ * L'apprenti se déplace entre les temples pour échanger des cristaux en fonction de conditions spécifiques.
+ */
 public class AlgoTri {
 
     private ApprentiOrdonnateur apprenti;
 
+    /**
+     * Construit une instance de la classe AlgoTri.
+     * Initialise l'apprenti et traite chaque temple pour gérer les échanges de cristaux.
+     */
     public AlgoTri() {
         System.out.println("Algo tri -> constructeur");
         apprenti = VBoxRoot.getApprenti();
@@ -23,23 +31,20 @@ public class AlgoTri {
             Cristal cristalTemple = currentTemple.getCristal();
             posiApprenti.deplacementUneCase(currentTemple.getPosiTemple());
             System.out.println(posiApprenti);
-                if (cristalTemple.equalsCoul(currentTemple.getCoulTemple()) && !currentTemple.equalsCoul(currentTemple.getCoulTemple())) {
-                    try {
-                        System.out.println("Apprenti va a la position " + currentTemple.getPosiTemple());
-                        while (!posiApprenti.equals(currentTemple.getPosiTemple())) {
-                            posiApprenti.deplacementUneCase(currentTemple.getPosiTemple());
-                        }
-                        apprenti.echangeCristal();
-
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-
+            if (cristalTemple.equalsCoul(currentTemple.getCoulTemple()) && !currentTemple.equalsCoul(currentTemple.getCoulTemple())) {
+                try {
+                    System.out.println("Apprenti va à la position " + currentTemple.getPosiTemple());
+                    while (!posiApprenti.equals(currentTemple.getPosiTemple())) {
+                        posiApprenti.deplacementUneCase(currentTemple.getPosiTemple());
                     }
+                    apprenti.echangeCristal();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
+            }
         }
     }
 }
-
 
 
 
