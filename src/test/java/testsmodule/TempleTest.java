beacfd71dd5a8dc21d9f68571d8c09c05596baad
younes <younesazirgui@gmail.com>
2024-06-04 15:24:48 +0200

    test de la classe Temple

--- src/test/java/testsmodule/TempleTest.java
+++ src/test/java/testsmodule/TempleTest.java
@@ -2,9 +2,7 @@ package testsmodule;
 
 import static org.junit.Assert.assertEquals;
 
-import javafx.geometry.Pos;
 import modele.Position;
-import vue.ConstantesCanvas;
 import modele.Temple;
 import modele.Cristal;
 import org.junit.Before;
@@ -14,32 +12,55 @@ public class TempleTest {
     private Temple temple;
 
 
+    /**
+     * Configure le test.
+     * Cette méthode est appelée avant chaque méthode de test pour initialiser l'instance de `Temple` avec des valeurs spécifiques.
+     */
     @Before
     public void setUp() {
-        Position position = new Position(10,10);
+        Position position = new Position(10, 10);
         Integer couleur = 6;
         Cristal cristal = new Cristal(6);
         temple = new Temple(position, couleur, cristal);
     }
+
+    /**
+     * Teste la méthode getPosiTemple.
+     * Vérifie si la position du temple est correctement récupérée.
+     */
 
     @Test
     public void testGetPosiTemple() {
         assertEquals(new Position(10,10), temple.getPosiTemple());
     }
 
+
+    /**
+     * Teste la méthode getCoulTemple.
+     * Vérifie si la couleur du temple est correctement récupérée.
+     */
     @Test
     public void testGetCoulTemple() {
         assertEquals(6, temple.getCoulTemple());
     }
 
+    /**
+     * Teste la méthode getCristal.
+     * Vérifie si le cristal du temple est correctement récupéré.
+     */
     @Test
     public void testGetCristal() {
-        assertEquals(6, temple.getCristal());
+        assertEquals(new Cristal(6), temple.getCristal());
     }
 
+    /**
+     * Teste la méthode toString.
+     * Vérifie si la représentation sous forme de chaîne de caractères du temple est correcte.
+     */
     @Test
     public void testToString() {
-        assertEquals("{Posi : " + temple.getPosiTemple() + " ; CoulTemple : " + temple.getCoulTemple() + " ; Cristal : " + temple.getCristal() + "}", temple.toString());
+        String expectedString = "{Posi : " + temple.getPosiTemple() + " ; CoulTemple : " + temple.getCoulTemple() + " ; Cristal : " + temple.getCristal() + "}";
+        assertEquals(expectedString, temple.toString());
     }
 
 }
